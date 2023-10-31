4c0d8958268202c9be1d3a8bfe087977033076d2
Bedanta Kataki <132095134+r4inr3aper@users.noreply.github.com>
2023-11-01 03:40:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 const hero = document.getElementsByClassName("hero");
 const url = "https://api.github.com/users/";
 const searchBox = document.querySelector("#search");
-const getUser = async (username) => {
-  
-  const res = await fetch(url + username);
-  const data = await res.json();
-  console.log(data);
-  let imgcon=document.createAttribute('imgcon');
-  imgcon.value=`${data.avatar}`
-
-  var attr = document.createAttribute('step');
-  attr.value="any";
-
-  document.getElementById('avatar').src=`${data.avatar_url}`
-  document.getElementById('username').innerHTML=`${data.login}`
-  document.getElementById('username2').innerHTML=`${data.login}`
-  document.getElementById('name').innerHTML=`${data.name}`
-  document.getElementById('bio').innerHTML=`${data.bio}`
-  document.getElementById('bio2').innerHTML=`${data.bio}`
-  document.getElementById('location').innerHTML=`${data.location}`
-  document.getElementById('public_repos').innerHTML=`${data.public_repos}`
-  document.getElementById('followers').innerHTML=`${data.followers}`
-  document.getElementById('following').innerHTML=`${data.following}`
-  document.getElementById('location').innerHTML=`${data.location}`
-  document.getElementById('html_url').innerHTML=`${data.html_url}`
-}
 const formSubmit = () => {
   if (searchBox.value !== "") {
     getUser(searchBox.value);
   }
   return false;
-};
+}
+const getUser = async (username) => {
+  
+  fetch(url + username)
+  .then((res) =>{ return res.json()})
+  .then((data)=>{
+    console.log(data);
+  
+  
+    document.getElementById('avatar').src=`${data.avatar_url}`
+    document.getElementById('username').innerHTML=`Username: ${data.login}`
+    document.getElementById('username2').innerHTML=`${data.login}`
+    document.getElementById('location1').innerHTML=`Location: ${data.location}`
+    document.getElementById('bio').innerHTML=`Bio: ${data.bio}`
+    document.getElementById('bio2').innerHTML=`${data.bio}`
+    document.getElementById('location').innerHTML=`${data.location}`
+    document.getElementById('public_repos').innerHTML=`${data.public_repos}`
+    document.getElementById('followers').innerHTML=`${data.followers}`
+    document.getElementById('following').innerHTML=`${data.following}`
+    document.getElementById('location').innerHTML=`${data.location}`
+    document.getElementById('html_url').innerHTML=`${data.html_url}`
+  }
+  )
+  .catch((error)=>{
+   alert("error")
+  })
+}
