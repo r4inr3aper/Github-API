913f4e88e3326b0e922d053ad0d5d3ff19931de5
Bedanta Kataki <132095134+r4inr3aper@users.noreply.github.com>
2023-11-01 01:17:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,36 @@ const hero = document.getElementsByClassName("hero");
 const url = "https://api.github.com/users/";
 const searchBox = document.querySelector("#search");
 const getUser = async (username) => {
+  
   const res = await fetch(url + username);
   const data = await res.json();
   console.log(data);
-  const card = `
-     <div class="left">
-          <div class="img">
-               <img
-                 src="${data.avatar_url}"
-               />
-               </div>
-   
-             <div class="details">
-               <h2>${data.login}</h2>
-               <h3>${data.name}</h3>
-               <p>${data.bio}</p>
-             </div>
-          </div>
-  
-           <div class="right">
-             <p>
-               <h4>Breaking News</h4>In the heart of the code cosmos, we introduce
+  let imgcon=document.createAttribute('imgcon');
+  imgcon.value=`${data.avatar}`
+
+  var attr = document.createAttribute('step');
+  attr.value="any";
+
+  document.getElementById('avatar').src=`${data.avatar_url}`
+  document.getElementById('username').innerHTML=`${data.login}`
+  document.getElementById('username2').innerHTML=`${data.login}`
+  document.getElementById('name').innerHTML=`${data.name}`
+  document.getElementById('bio').innerHTML=`${data.bio}`
+  document.getElementById('bio2').innerHTML=`${data.bio}`
+  document.getElementById('location').innerHTML=`${data.location}`
+  document.getElementById('public_repos').innerHTML=`${data.public_repos}`
+  document.getElementById('followers').innerHTML=`${data.followers}`
+  document.getElementById('following').innerHTML=`${data.following}`
+  document.getElementById('location').innerHTML=`${data.location}`
+  document.getElementById('html_url').innerHTML=`${data.html_url}`
+}
+const formSubmit = () => {
+  if (searchBox.value !== "") {
+    getUser(searchBox.value);
+  }
+  return false;
+};
+
                you to ${data.login}, a coding virtuoso known for their ${data.public_repos} repositories, drawing in ${data.followers}
                followers and following ${data.following}.
                Hailing from ${data.location}, they're the subject of our next profile
